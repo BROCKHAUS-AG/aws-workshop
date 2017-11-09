c30a2788b1a8ab03fa721571420256d98d7c1a40
Fabian Raetz <fabian.raetz@gmail.com>
2017-11-09 19:39:32 +0100

    move src files under src

--- packages/todomvc-frontend/src/api/todos.js
+++ packages/todomvc-frontend/src/api/todos.js
@@ -1,4 +1,4 @@
-const TODOS_API = 'https://rqvmwff5ia.execute-api.eu-central-1.amazonaws.com/Prod/todos';
+const TODOS_API = 'https://qjmo23obx4.execute-api.eu-central-1.amazonaws.com/Prod/todos';
 
 export async function getTodos() {
     const data = await fetch(TODOS_API);
